94b14aaca23823a6e0d48f640479cf2ed9f78de6
Lutfi Maulana <lutfimaulana.fi@gmail.com>
2022-03-29 20:04:52 +0700

    convert sign in page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import "../styles/homepage.css";
 import "../styles/detail.css";
 import "../styles/checkout.css";
 import "../styles/complete-checkout.css";
+import "../styles/sign-in.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
