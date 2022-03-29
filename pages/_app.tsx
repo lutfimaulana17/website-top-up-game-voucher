93f11d14a8a92dcfca43df5fe11b2f697b3f080a
Lutfi Maulana <lutfimaulana.fi@gmail.com>
2022-03-29 20:39:15 +0700

    convert sign up photo page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import "../styles/checkout.css";
 import "../styles/complete-checkout.css";
 import "../styles/sign-in.css";
 import "../styles/sign-up.css";
+import "../styles/sign-up-photo.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
