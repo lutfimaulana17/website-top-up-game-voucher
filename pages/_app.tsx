5171d046fe5deb4e223a2fff2ebd2c8878386f41
Lutfi Maulana <lutfimaulana.fi@gmail.com>
2022-03-30 20:26:19 +0700

    convert transactions detail

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,7 @@ import "../styles/404-not-found.css";
 import "../styles/sidebar.css";
 import "../styles/overview.css";
 import "../styles/transactions.css";
+import "../styles/transactions-detail.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
