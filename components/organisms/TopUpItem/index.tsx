3c853d4c4e52594e0215499a5a36d75ad3df428d
Lutfi Maulana <lutfimaulana.fi@gmail.com>
2022-04-03 11:00:35 +0700

    refactor detail page

---
+++ components/organisms/TopUpItem/index.tsx
@@ -0,0 +1,41 @@
+interface TopUpItemProps {
+  type: "desktop" | "mobile";
+}
+export default function TopUpItem(props: TopUpItemProps) {
+  const { type } = props;
+  if (type === "desktop") {
+    return (
+      <div className="pb-50 d-md-block d-none">
+        <h2 className="text-4xl fw-bold color-palette-1 text-start mb-10 mt-10">
+          Mobile Legends:
+          <br />
+          The New Battle 2021
+        </h2>
+        <p className="text-lg color-palette-2 mb-0">Category: Mobile</p>
+      </div>
+    );
+  }
+  return (
+    <div className="row align-items-center">
+      <div className="col-md-12 col-4">
+        <img
+          src="/img/Thumbnail-3.png"
+          width="280"
+          height="380"
+          className="img-fluid"
+          alt=""
+        />
+      </div>
+      <div className="col-md-12 col-8 d-md-none d-block">
+        <h2 className="text-xl fw-bold color-palette-1 text-start mb-10">
+          Mobile Legends:
+          <br />
+          The New Battle 2021
+        </h2>
+        <p className="text-sm color-palette-2 text-start mb-0">
+          Category: Mobile
+        </p>
+      </div>
+    </div>
+  );
+}
