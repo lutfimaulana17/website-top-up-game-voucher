f025b3f8cd95efdf0061e6720648bcdce0ced2af
account1 <mochammad.lutfi@bluebirdgroup.com>
2022-04-27 21:50:05 +0700

    sample integration next js

--- components/molecules/GameItem/index.tsx
+++ components/molecules/GameItem/index.tsx
@@ -3,12 +3,7 @@ import Link from "next/link";
 export interface GameItemProps {
   title: string;
   category: string;
-  thumbnail:
-    | "/img/Thumbnail-1.png"
-    | "/img/Thumbnail-2.png"
-    | "/img/Thumbnail-3.png"
-    | "/img/Thumbnail-4.png"
-    | "/img/Thumbnail-5.png";
+  thumbnail: string;
 }
 export default function GameItem(props: GameItemProps) {
   const { title, category, thumbnail } = props;
--- components/organisms/FeaturedGame/index.tsx
+++ components/organisms/FeaturedGame/index.tsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import GameItem from "../../molecules/GameItem";
 
 export default function FeaturedGame() {
+  const [gameList, setGameList] = useState([]);
+
+  useEffect(async () => {
+    const response = await axios.get(
+      "https://admin-website-top-up-game.herokuapp.com/api/v1/players/landingpage"
+    );
+    setGameList(response.data.data);
+  }, []);
+
   return (
     <section className="featured-game pt-50 pb-50">
       <div className="container-fluid">
@@ -12,31 +23,14 @@ export default function FeaturedGame() {
           className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4"
           data-aos="fade-up"
         >
-          <GameItem
-            title="Super Mechs"
-            category="Mobile"
-            thumbnail="/img/Thumbnail-1.png"
-          />
-          <GameItem
-            title=" Call of Duty: Modern"
-            category="Mobile"
-            thumbnail="/img/Thumbnail-2.png"
-          />
-          <GameItem
-            title="Mobile Legends"
-            category="Mobile"
-            thumbnail="/img/Thumbnail-3.png"
-          />
-          <GameItem
-            title="Clash of Clans"
-            category="Mobile"
-            thumbnail="/img/Thumbnail-4.png"
-          />
-          <GameItem
-            title="Valorant"
-            category="Desktop"
-            thumbnail="/img/Thumbnail-5.png"
-          />
+          {gameList.map((item) => (
+            <GameItem
+              key={item._id}
+              title={item.name}
+              category={item.category.name}
+              thumbnail={`https://admin-website-top-up-game.herokuapp.com/uploads/${item.thumbnail}`}
+            />
+          ))}
         </div>
       </div>
     </section>
