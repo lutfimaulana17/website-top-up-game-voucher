e53caace1a3c006e1f2c503180d0dfdb63b8d8f7
account1 <mochammad.lutfi@bluebirdgroup.com>
2022-05-05 13:39:03 +0700

    integration game item

--- components/organisms/CheckoutItem/index.tsx
+++ components/organisms/CheckoutItem/index.tsx
@@ -1,17 +1,37 @@
+import { useEffect, useState } from "react";
+
 export default function CheckoutItem() {
+  const [dataItem, setDataItem] = useState({
+    thumbnail: "",
+    name: "",
+    category: {
+      name: "",
+    },
+  });
+
+  useEffect(() => {
+    const dataFromLocal = localStorage.getItem("data-item");
+    const dataItemLocal = JSON.parse(dataFromLocal!);
+    setDataItem(dataItemLocal);
+  }, []);
+
+  const IMG = process.env.NEXT_PUBLIC_IMG;
   return (
     <div className="game-checkout d-flex flex-row align-items-center pt-md-50 pb-md-50 pt-30 pb-30">
       <div className="pe-4">
         <div className="cropped">
-          <img src="/img/Thumbnail-3.png" className="img-fluid" alt="" />
+          <img
+            src={`${IMG}/${dataItem.thumbnail}`}
+            className="img-fluid"
+            alt=""
+          />
         </div>
       </div>
       <div>
-        <p className="fw-bold text-xl color-palette-1 mb-10">
-          Mobile Legends:
-          <br /> The New Battle 2021
+        <p className="fw-bold text-xl color-palette-1 mb-10">{dataItem.name}</p>
+        <p className="color-palette-2 m-0">
+          Category: {dataItem.category.name}
         </p>
-        <p className="color-palette-2 m-0">Category: Mobile</p>
       </div>
     </div>
   );
