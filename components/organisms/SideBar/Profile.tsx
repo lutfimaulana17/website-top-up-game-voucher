0d9afbd91b5b5497ad08d185989af45a5a49009e
account1 <mochammad.lutfi@bluebirdgroup.com>
2022-05-05 20:42:23 +0700

    integration member profile user

--- components/organisms/SideBar/Profile.tsx
+++ components/organisms/SideBar/Profile.tsx
@@ -1,14 +1,41 @@
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import jwtDecode from "jwt-decode";
+import { JWTPayloadTypes, UserTypes } from "../../../services/data-types";
+
 export default function Profile() {
+  const [user, setUser] = useState({
+    avatar: "",
+    email: "",
+    id: "",
+    name: "",
+    username: "",
+  });
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    if (token) {
+      const jwtToken = atob(token);
+      const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+      const userPlayer: UserTypes = payload.player;
+      const IMG = process.env.NEXT_PUBLIC_IMG;
+      userPlayer.avatar = `${IMG}/${userPlayer.avatar}`;
+      setUser(userPlayer);
+    }
+  }, []);
+
   return (
     <div className="user text-center pb-50 pe-30">
       <img
-        src="/img/avatar-1.png"
+        src={user.avatar}
         width="90"
         height="90"
-        className="img-fluid mb-20"
+        className="mb-20"
+        alt="profile"
+        style={{ borderRadius: "100%", objectFit: "cover" }}
       />
-      <h2 className="fw-bold text-xl color-palette-1 m-0">Shayna Anne</h2>
-      <p className="color-palette-2 m-0">shayna@anne.com</p>
+      <h2 className="fw-bold text-xl color-palette-1 m-0">{user.name}</h2>
+      <p className="color-palette-2 m-0">{user.email}</p>
     </div>
   );
 }
