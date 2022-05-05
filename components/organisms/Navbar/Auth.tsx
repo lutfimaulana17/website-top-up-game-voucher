5460858ed9acf4bd8facd3a65c1d6d22c089ec8b
account1 <mochammad.lutfi@bluebirdgroup.com>
2022-05-05 08:50:12 +0700

    create condition login navbar

--- components/organisms/Navbar/Auth.tsx
+++ components/organisms/Navbar/Auth.tsx
@@ -1,10 +1,31 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import jwt_decode from "jwt-decode";
 
-interface AuthProps {
-  isLogin?: boolean;
-}
-export default function Auth(props: Partial<AuthProps>) {
-  const { isLogin } = props;
+export default function Auth() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [user, setUser] = useState({
+    avatar: "",
+    email: "",
+    id: "",
+    name: "",
+    username: "",
+  });
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    if (token) {
+      const jwtToken = atob(token);
+      const payload = jwt_decode(jwtToken);
+      const userPlayer = payload.player;
+      const IMG = process.env.NEXT_PUBLIC_IMG;
+      userPlayer.avatar = `${IMG}/${userPlayer.avatar}`;
+      setIsLogin(true);
+      setUser(userPlayer);
+    }
+  }, []);
+
   if (isLogin) {
     return (
       <li className="nav-item my-auto dropdown d-flex">
@@ -19,7 +40,7 @@ export default function Auth(props: Partial<AuthProps>) {
             aria-expanded="false"
           >
             <img
-              src="/img/avatar-1.png"
+              src={user.avatar}
               className="rounded-circle"
               width="40"
               height="40"
